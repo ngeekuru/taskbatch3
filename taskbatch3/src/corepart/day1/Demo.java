cf2055a38ad3aa271c3eee469a8d6c0b83af058a
ngeekuru <ngeekuru@tnsif.org>
2026-08-19 10:48:37 +0530

    day 1 program

---
+++ taskbatch3/src/corepart/day1/Demo.java
@@ -0,0 +1,33 @@
+package corepart.day1;
+
+public class Demo {
+	public static void main(String args[])
+	{
+		char c = 'a';
+		boolean b = true;
+		byte bt = 123;
+		short s = 12345;
+		int i = 234567;
+		long l = 835687;
+		float f = 23.67f;
+		double d = 32765.43;
+		System.out.println("the character value is : "+c);
+		System.out.println(b);
+		System.out.println(bt);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		
+		
+		int aa = 10;
+		int bb = 20;
+		int cc = aa+bb;
+		System.out.println(cc);
+		
+		
+		
+	}
+
+}
